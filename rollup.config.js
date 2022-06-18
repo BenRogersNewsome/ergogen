ead22d0975777535bef2175014a2e7c8ed9e678d
Ben Rogers-Newsome <benrogersnewsome@gmail.com>
2022-06-18 18:02:17 +0100

    Add babel to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,49 @@
 import pkg from './package.json'
 import json from '@rollup/plugin-json'
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs'
 
-export default {
-  input: 'src/ergogen.js',
-  external: ['makerjs', 'js-yaml', 'mathjs', 'kle-serial', '@jscad/openjscad', 'semver'],
-  output: {
-    name: 'ergogen',
-    file: 'dist/ergogen.js',
-    format: 'umd',
-    banner: `/*!\n * Ergogen v${pkg.version}\n * https://zealot.hu/ergogen\n */\n`,
-    globals: {
-      'makerjs': 'makerjs',
-      'js-yaml': 'jsyaml',
-      'mathjs': 'math',
-      'kle-serial': 'kle',
-      '@jscad/openjscad': 'myjscad',
-      'semver': 'semver'
+const external = ['makerjs', 'js-yaml', 'mathjs', 'kle-serial', '@jscad/openjscad', 'semver']
+const plugins = [
+  json(),
+  babel({ babelHelpers: 'bundled' }),
+  commonjs(),
+]
+
+export default [{
+    input: 'src/ergogen.js',
+    external,
+    output: {
+      name: 'ergogen',
+      file: 'dist/ergogen.js',
+      format: 'umd',
+      banner: `/*!\n * Ergogen v${pkg.version}\n * https://zealot.hu/ergogen\n */\n`,
+      globals: {
+        'makerjs': 'makerjs',
+        'js-yaml': 'jsyaml',
+        'mathjs': 'math',
+        'kle-serial': 'kle',
+        '@jscad/openjscad': 'myjscad',
+        'semver': 'semver'
+      },
+      plugins,
+    },
+  },
+  {
+    input: 'src/cli.js',
+    external,
+    output: {
+      name: 'ergogen',
+      file: 'dist/cli.js',
+      format: 'umd',
+      banner: `/*!\n * Ergogen v${pkg.version}\n * https://ergogen.xyz\n */\n`,
+      globals: {
+        'makerjs': 'makerjs',
+        'js-yaml': 'jsyaml',
+        'mathjs': 'math',
+        'kle-serial': 'kle'
+      },
+      plugins,
     }
   },
-  plugins: [
-    json(),
-    commonjs()
-  ]
-}
+]
